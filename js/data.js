496f33187211bdf9b89f494d8e2bb2474e505d44
visay <vsvang@gmail.com>
2022-06-18 12:07:11 -0400

    June 18 update

---
+++ js/data.js
@@ -0,0 +1,18 @@
+//character data
+const characterData = {
+  hero: {
+    name: "Wizard",
+    avatar: "/rpg_image/wizard.jpg",
+    health: 60,
+    diceCount: 3,
+  },
+
+  monster: {
+    name: "Orc",
+    avatar: "/rpg_image/orc.jpg",
+    health: 40,
+    diceCount: 1,
+  },
+};
+
+export default characterData;
--- js/rpg_game.js
+++ js/rpg_game.js
@@ -1,83 +1,11 @@
-//get dice array
-/*function getDiceRollArray(diceCount) {
-  return new Array(diceCount).fill(0).map(() => {
-    return Math.floor(Math.random() * 6) + 1;
-  });
-}
-//get dice HTML function
-function getDiceHtml(diceCount) {
-  return getDiceRollArray(diceCount)
-    .map((num) => {
-      return `<div class="dice">${num}</div>`;
-    })
-    .join("");
-}
-
-//hero object
-const hero = {
-  elementId: "hero",
-  name: "Wizard",
-  avatar: "/rpg_image/wizard.jpg",
-  health: 60,
-
-  diceCount: 3,
-};
-
-//monster object
-const monster = {
-  elementId: "monster",
-  name: "Orc",
-  avatar: "/rpg_image/orc.jpg",
-  health: 40,
-
-  diceCount: 2,
-};
-
+//import data from data.js
+import characterData from "./data.js";
+import Character from "./Character.js";
 //render function
-function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceCount } = data;
-  const diceHTML = getDiceHtml(diceCount);
-
-  document.getElementById(elementId).innerHTML = `
-    <div class="character-card">
-            <h4 class="name">${name} </h4>
-            <img class="avatar" src="${avatar}"/>
-            <p class="health">health: <b>${health}</b></p>
-            <div class="dice-container">${diceHTML}</div>
-        </div> 
-    `;
+function render() {
+  document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
+  document.getElementById("monster").innerHTML = orc.getCharacterHtml();
 }
-renderCharacter(hero);
-renderCharacter(monster);
-*/
-
-const hotel1 = {
-  name: "Starfield",
-  rooms: 30,
-  stars: "⭐⭐⭐",
-  costPerNightChildren: 150,
-  costPerNightAdult: 250,
-};
-const hotel2 = {
-  name: "Sandy Beach",
-  rooms: 40,
-  stars: "⭐⭐⭐⭐⭐",
-  costPerNightChildren: 250,
-  costPerNightAdult: 350,
-};
-
-function NationalParkHotels(data) {
-  this.name = data.name;
-  this.rooms = data.rooms;
-  this.stars = data.stars;
-  this.cost = data.cost;
-  this.summariseHotel = function () {
-    const totalCost =
-      this.costPerNightChildren * 2 + this.costPerNightAdult * 2;
-    console.log(
-      `A one night at the ${hotel1.name} for two adults and two children costs a total of ${totalCost}.`
-    );
-  };
-}
-const starField = new NationalParkHotels(hotel1);
-starField.summariseHotel();
+const wizard = new Character(characterData.hero);
+const orc = new Character(characterData.monster);
+render();
